a8a4ac5d35c867fb54aab1868520957a2d2de0c2
rian <rianrgomes@gmail.com>
2024-04-16 14:42:02 -0300

    fix discord oauth link

--- src/routes/getLink.ts
+++ src/routes/getLink.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from "fastify";
 
 export async function GetLink(app: FastifyInstance){
   app.get('/auth/discord', async (req, res) =>{
-    // res.redirect('https://discord.com/oauth2/authorize?client_id=1229801593415663628&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fdiscord&scope=email+identify+guilds+gdm.join+guilds.join+connections')
+    res.redirect('https://discord.com/oauth2/authorize?client_id=1229846501207969914&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fsession&scope=email+identify+gdm.join+guilds+guilds.join+connections')
     res.send({ msg: 'Logged succesfully!' })
   })
 }
