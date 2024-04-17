57196fd188e7c240b1a029547970fbe04e0492c3
rian <rianrgomes@gmail.com>
2024-04-17 10:27:23 -0300

    feat: redirect user if wrong code

--- src/routes/getSession.ts
+++ src/routes/getSession.ts
@@ -6,44 +6,46 @@ export async function getSession(app: FastifyInstance){
   app.get('/session', async (req: any, res) =>{
     const { code } = req.query;
     
-    if(code){
-      const formData = new url.URLSearchParams({
-        client_id: process.env.CLIENT_ID,
-        client_secret: process.env.CLIENT_SECRET,
-        grant_type: 'authorization_code',
-        code: code.toString(),
-        redirect_uri: 'http://localhost:5000/session'
-      })
+    if(!code)
+      res.redirect('http://localhost:5000/auth/discord')
 
-      const output = await axios.post('https://discord.com/api/v10/oauth2/token', formData, {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
-      })
-      if(output.data){
-        const access = output.data.access_token
-        const userInfo = await axios.get('https://discord.com/api/v10/users/@me', {
-          headers: {
-            'Authorization': `Bearer ${access}`
-          }
-        })
+    const formData = new url.URLSearchParams({
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      grant_type: 'authorization_code',
+      code: code.toString(),
+      redirect_uri: 'http://localhost:5000/session'
+    })
 
-        const formDataForRefresh = new url.URLSearchParams({
-          client_id: process.env.CLIENT_ID,
-          client_secret: process.env.CLIENT_SECRET,
-          grant_type: 'refresh_token',
-          refresh_token: output.data.refresh_token
-        })
-  
-        const refresh = await axios.post('https://discord.com/api/v10/oauth2/token', formDataForRefresh, {
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          }
-        })
-
-        console.log(output.data, userInfo.data, refresh.data)
+    const output: any = await axios.post('https://discord.com/api/v10/oauth2/token', formData, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
       }
-    }
+    }).catch(err =>{
+      res.redirect('http://localhost:5000/auth/discord')
+    })
+
+    const access = output.data.access_token
+    const userInfo = await axios.get('https://discord.com/api/v10/users/@me', {
+      headers: {
+        'Authorization': `Bearer ${access}`
+      }
+    })
+
+    const formDataForRefresh = new url.URLSearchParams({
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      grant_type: 'refresh_token',
+      refresh_token: output.data.refresh_token
+    })
+
+    const refresh = await axios.post('https://discord.com/api/v10/oauth2/token', formDataForRefresh, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+
+    console.log(output.data, userInfo.data, refresh.data)
     
     res.send({ msg: 'logged' })
   })
